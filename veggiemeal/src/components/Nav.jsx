d62684defc7dc80562225e85bea1b1ebaa435a86
DevoAlex <alex.peluso8@gmail.com>
2022-09-30 00:43:56 +0200

    sidebar completed

--- veggiemeal/src/components/Nav.jsx
+++ veggiemeal/src/components/Nav.jsx
@@ -3,11 +3,20 @@ import styled from "styled-components";
 import { FaBars } from "react-icons/fa";
 import { AiOutlineClose } from "react-icons/ai";
 import { Link } from "react-router-dom";
+import vegan from '../images/vegetarian.png'
+import home from '../images/home.png'
+import fast from '../images/fast.png'
+import star from '../images/star.png'
+import glutenFree from '../images/gluten-free.png'
+
 
 function Nav() {
   const [isOpen, setIsOpen] = useState(false);
 
-  const showSidebar = () => setIsOpen(!isOpen);
+  const showSidebar = () => {
+    setIsOpen(!isOpen);
+    console.log(isOpen);
+  };
 
   return (
     <>
@@ -16,43 +25,41 @@ function Nav() {
           <Bars onClick={showSidebar} />
         </Link>
       </Navbar>
-      <Sidebar>
-        <ul onClick={showSidebar}>
-          <li>
-            <Link to="#">
-              <AiOutlineClose />
-            </Link>
-          </li>
-          <li>
-            <Link to="/">
-              <p>Home</p>
-            </Link>
-          </li>
-          <li>
-            <Link to="/saved">
-              <p>Saved</p>
-            </Link>
-          </li>
-          <li>
-            <Link to="/vegan">
-              <p>Vegan</p>
-            </Link>
-          </li>
-          <li>
-            <Link to="/glutenfree">
-              <p>Gluten</p>
-            </Link>
-          </li>
-          <li>
-            <Link to="/fast">
-              <p>Fast</p>
-            </Link>
-          </li>
+      {isOpen ? (
+        <SSidebar isOpen={isOpen} onClick={showSidebar}>
+          <SSlink to="#">
+            <AiOutlineClose style={{height: '20px', width: '20px'}}/>
+            <p>Close</p>
+          </SSlink>
 
-        </ul>
-      </Sidebar>
+          <SSlink to="/">
+          <img src={home} />
+            <p>Home</p>
+          </SSlink>
 
-      
+          <SSlink to="/saved">
+          <img src={star} />
+            <p>Saved</p>
+          </SSlink>
+
+          <SSlink to="/vegan">
+          <img src={vegan} />
+            <p>Vegan</p>
+          </SSlink>
+
+          <SSlink to="/glutenfree">
+          <img src={glutenFree} />
+            <p>Gluten</p>
+          </SSlink>
+
+          <SSlink to="/fast">
+          <img src={fast} />
+            <p>Fast</p>
+          </SSlink>
+        </SSidebar>
+      ) : (
+        ""
+      )}
     </>
   );
 }
@@ -66,12 +73,46 @@ const Navbar = styled.div`
   margin: 0px 20px;
 `;
 
-const Sidebar = styled.nav``;
+const SSidebar = styled.div`
+  background-color: #307351;
+  position: fixed;
+  height: 100%;
+  top: 0px;
+  left: 0px;
+  width: 200px;
+  left: ${props => props.isOpen ? '0' : '-100%'};
+  animation: showSidebar .4s;
+  @keyframes showSidebar {
+    from {
+      opacity: 0;
+      width: 0;
+    }
+    to {
+      opacity: 1;
+      width: 200px;
+    }}
+`;
 
 const Bars = styled(FaBars)`
   height: 25px;
   width: 30px;
   color: white;
+  cursor: pointer;
+  text-align: center;
 `;
+
+const SSlink = styled(Link)`
+display: flex;
+align-items: center;
+gap: 10px;
+margin-left: 15px;
+img {
+  width: 20px;
+  height: 20px;
+}
+&:hover {
+  color: white;
+}
+`
 
 export default Nav;
